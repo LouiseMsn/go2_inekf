b09cebe612fe8fa4c224f52461ba643c97622a40
LouiseMsn <louise.manson@inria.fr>
2025-02-04 15:57:10 +0100

    added classes RobotState and NoiseParameter

--- scripts/inekf.py
+++ scripts/inekf.py
@@ -9,6 +9,12 @@ from unitree_go.msg import LowState
 import numpy as np
 import math
 
+# TODO from RobotState import * 
+
+# ==============================================================================
+# Main Class
+# ==============================================================================
+
 class Inekf(Node):
 
     def __init__(self):
@@ -20,19 +26,29 @@ class Inekf(Node):
                                                     '/lowstate',
                                                     self.listener_callback,
                                                     10)
-        # filter global variables
+        
+        # Filter and global variables ==========================================
         self.g_ = np.array(([0,0,-9.81])) # Gravity vector
         
         self.DT_MIN = 1e-6
         self.DT_MAX = 1
         self.dt = 0
 
-        # Init robot and filter state
-        self.R = np.identity(3) # TODO: verify the imu frame is in the same orientation as the state frame
-        self.v = np.zeros((3))
-        self.p = np.zeros((3))
-        self.bg = np.zeros((3)) # bg = Bias Gyroscope
-        self.ba = np.zeros((3)) # ba = Bias Accelerometer
+        # Init Robot and Filter state ==========================================
+        # initiated by constructor in OG class
+        state_ = RobotState()
+
+        R0 = np.identity(3) # TODO: verify the imu frame is in the same orientation as the state frame
+        v0 = np.zeros((3))
+        p0 = np.zeros((3))
+        bg0 = np.zeros((3)) # bg = Bias Gyroscope
+        ba0 = np.zeros((3)) # ba = Bias Accelerometer
+
+        self.state_.setRotation(R0);
+        self.state_.setVelocity(v0);
+        self.state_.setPosition(p0);
+        self.state_.setGyroscopeBias(bg0);
+        self.state_.setAccelerometerBias(ba0);
 
         #TODO implement noise
         # Q = covariance matrix
@@ -51,7 +67,7 @@ class Inekf(Node):
         # self.Ql_ = np.identity((3)) * (1e-10*1e-10) # bias landmarks
         self.Qc_ = np.identity((3)) * (1e-10*1e-6) # bias contacts
 
-        # Data from go2
+        # Data from go2 ========================================================
         self.imu_measurement_ = np.zeros((6))
         self.imu_measurement_prev_ = np.zeros((6))
         self.feet_contacts_ = np.zeros((4)) 
@@ -62,10 +78,10 @@ class Inekf(Node):
 
 
     def listener_callback(self, state_msg):
-        # Get timestamp # TODO
-        self.t = state_msg.tick * 0.001 #! convert from ms to s like it seems to be in OG code
+        # TODO Get timestamp 
+        self.t = state_msg.tick * 0.001 # convert from ms to s like it seems to be in OG code
 
-        # IMU measurement - used for propagation
+        # IMU measurement - used for propagation ===============================
         self.imu_measurement_[0] = state_msg.imu_state.gyroscope[0]
         self.imu_measurement_[1] = state_msg.imu_state.gyroscope[1]
         self.imu_measurement_[2] = state_msg.imu_state.gyroscope[2]
@@ -87,13 +103,13 @@ class Inekf(Node):
             a = self.imu_measurement_prev_[3:] - self.ba # first three values of imu (accel) - accel bias
             
 
-            #! Eigen::MatrixXd X = state_.getX();
-            #! Eigen::MatrixXd P = state_.getP();
+            X = self.state_.getX();
+            P = self.state_.getP();
 
             # Extract State self.R self.v self.p
-            # Eigen::Matrix3d R = state_.getRotation();
-            # Eigen::Vector3d v = state_.getVelocity();
-            # Eigen::Vector3d p = state_.getPosition();
+            R = self.state_.getRotation();
+            v = self.state_.getVelocity();
+            p = self.state_.getPosition();
 
             # Strapdown IMU motion model
             phi = w*self.dt # vecteur (1,3)
@@ -102,16 +118,16 @@ class Inekf(Node):
             p_pred = self.p + self.v*self.dt + 0.5*(self.R*a + self.g_)*self.dt*self.dt; # vecteur (1,3)
 
             # Set new state (bias has constant dynamics)
-            # state_.setRotation(R_pred);
-            # state_.setVelocity(v_pred);
-            # state_.setPosition(p_pred);
-            self.R = R_pred
-            self.v = v_pred
-            self.p = p_pred
+            self.state_.setRotation(R_pred);
+            self.state_.setVelocity(v_pred);
+            self.state_.setPosition(p_pred);
+            
 
            
 
-            # # ---- Linearized invariant error dynamics -----
+            # Linearized invariant error dynamics ==============================
+            
+
             # int dimX = state_.dimX();
             # int dimP = state_.dimP();
             # int dimTheta = state_.dimTheta();
@@ -151,12 +167,12 @@ class Inekf(Node):
             # # Set new covariance
             # state_.setP(P_pred);
 
-#!-----------------------END OF COPY PASTE---------------
+#! END OF COPY PASTE ============================================================
 
 
 
 
-        # CONTACT data : order changed to match URDF #? not sure if needed
+        # CONTACT data : order changed to match URDF #? not sure if needed =====
         self.feet_contacts_[0] = 1
         self.feet_contacts_[1] = 0
         self.feet_contacts_[2] = 3
@@ -164,7 +180,7 @@ class Inekf(Node):
 
         contacts = np.array((0,0))
 
-        # KINEMATIC data
+        # KINEMATIC data =======================================================
         quaternion = np.zeros((4))
         quaternion[0] = state_msg.imu_state.quaternion[0]
         quaternion[1] = state_msg.imu_state.quaternion[1]
@@ -185,14 +201,12 @@ class Inekf(Node):
         # Computes the vectorized exponential map for SO(3)
         TOLERANCE = 1e-10
         A = self.skew(w)
-
         theta = np.linalg.norm(w)
         
         if(theta < TOLERANCE):
             return np.identity(3)
         
         R = np.identity(3) + (math.sin(theta)/theta)*A + ((1-math.cos(theta))/(theta*theta))*A*A
-
         return R
     
 
@@ -200,10 +214,159 @@ class Inekf(Node):
         # convert vector to skew-symmetric matrix
         M = np.zeros((3,3))
         M = np.array([[0,-vector[2],vector[1]],
-             [vector[2],0,-vector[0]],
-             [-vector[1],vector[0],0]])
+                    [vector[2],0,-vector[0]],
+                    [-vector[1],vector[0],0]])
         return M
 
+
+
+# ==============================================================================
+# RobotState Class
+# ==============================================================================
+class RobotState():
+    def __init__(self):
+        # TODO add other constructors like in OG code
+        self.__X = np.identity(5)
+        self.__Theta = np.zeros((6,1))
+        self.__P = np.identity(15)
+
+
+# Getters Setters ===============================================================
+    def getX(self):
+        return self.__X
+    
+    def setX(self,X):
+        self.__X = X
+    
+    def getTheta(self):
+        return self.__Theta
+    
+    def setTheta(self, Theta):
+        self.__Theta = Theta
+    
+    def getP(self):
+        return self.__P
+    
+    def setP(self,P):
+        self.__P = P
+
+    # ROTATION : 3x3 matrix starting in (0,0)
+    def getRotation(self):
+        return self.__X[0:3,0:3]
+    
+    def setRotation(self, rot):
+        self.__X[0:3,0:3] = rot
+
+    # VELOCITY : 3x1 vector starting in (0,3) 
+    def getVelocity(self):
+        return self.__X[0:3,3:4]
+    
+    def setVelocity(self,vel):
+        self.__X[0:3,3:4] = vel
+    
+    # POSITION : 3x1 vector starting in (0,4)
+    def getPosition(self):
+        return self.__X[0:3,4:5]
+    
+    def setPosition(self, pos):
+        self.__X[0:3,4:5] = pos
+
+    # ---
+    def dimX(self):
+        return self.__X.shape[1]
+    
+    def dimTheta(self):
+        return self.__Theta.shape[0]
+    
+    def dimP(self):
+        return self.__P.shape[1]
+        
+    def getGyroscopeBiais(self):
+        return self.__Theta[0:3,0:1]
+
+    def setGyroscopeBiais(self,bg):
+        self.__Theta[0:3,0:1] = bg
+
+    def getAccelerometerBiais(self):
+        return self.__Theta[3:6,0:1]
+
+    def setAccelerometerBiais(self,ba):
+        self.__Theta[3:6,0:1] = ba
+
+    def copyDiag(self, n, BigX):
+        # Returns a (n*dimX,n*dimX) array where X is copied along the diagonal
+        # TODO find more elegant way for returning BigX (->pointers if converted to C++ ('_'))
+        dimX = self.dimX()
+        
+        for i in range(0,n+1):
+            startIndex = BigX.shape[0]
+            
+            new_BigX = np.zeros((startIndex + dimX, startIndex + dimX))
+            new_BigX[:BigX.shape[0], :BigX.shape[1]] = BigX
+           
+            new_BigX[ startIndex : startIndex+dimX , startIndex : startIndex+dimX] = self.__X
+
+            BigX=new_BigX
+
+        return BigX
+
+
+# ==============================================================================
+# NoiseParam Class
+# ==============================================================================
+class NoiseParam():
+    def __init__(self):
+
+        self.__Qg  = np.array((3,3))
+        self.__Qa  = np.array((3,3))
+        self.__Qbg = np.array((3,3))
+        self.__Qba = np.array((3,3))
+        self.__Ql  = np.array((3,3))
+        self.__Qc  = np.array((3,3))
+        
+
+        self.setGyroscopeNoise(0.01)
+        self.setAccelerometerNoise(0.1)
+        self.setGyroscopeBiasNoise(0.00001)
+        self.setAccelerometerBiasNoise(0.0001)
+        self.setLandmarkNoise(0.1)
+        self.setContactNoise(0.1)
+        
+
+# Getters Setters ==============================================================
+
+    def setGyroscopeNoise(self,value):
+        self.__Qg = np.identity(3)*value*value
+
+    def getGyroscopeCov(self,value):
+        return self.__Qg
+    
+    def setAccelerometerNoise(self,value):
+        self.__Qa = np.identity(3)*value*value
+    
+    def getAccelerometerCov(self):
+        return self.__Qa
+    
+    def setGyroscopeBiasNoise(self,value):
+        self.__Qbg = np.identity(3)*value*value
+
+    def getGyroscopeBiaisCov(self):
+        return self.__Qbg
+    
+    def setAccelerometerBiasNoise(self,value):
+        self.__Qba = np.identity(3)*value*value
+
+    def getAccelerometerBiaisCov(self):
+        return self.__Qba
+
+    def setContactNoise(self,value):
+        self.__Qc = np.identity(3)*value*value
+
+    def getContactCov(self):
+        return self.__Qc
+
+    #NOTE void setLandmarkNoise(double std) and  Eigen::Matrix3d getContactCov() not implemented beacause not used
+    
 
 
 
